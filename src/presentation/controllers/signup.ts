b1b05df600175722561e722f88c3e05b861f9b4b
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-01-22 17:10:28 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
 export class SignUpController {
   handle (httpRquest: HttpRequest): HttpResponse {
-    if (!httpRquest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRquest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRquest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
