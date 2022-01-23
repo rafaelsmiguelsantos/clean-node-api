85f7ce88eb8b33d1e57036d94402abe128edcd04
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-01-23 12:10:52 -0300

    refactor: move all error import in test for file index

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,7 @@
 import { SignUpController } from './signup'
 import { EmailValidator } from '../protocols/email-validator'
 
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, ServerError, InvalidParamError } from '../errors'
 
 interface SutTypes {
   sut: SignUpController
