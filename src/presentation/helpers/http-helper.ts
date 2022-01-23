6b737133dea2e5dd5a5e10fb6dc0560210f26af1
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-01-23 18:25:39 -0300

    refactor: move import errors

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,5 +1,5 @@
-import { ServerError } from '../errors/server-error'
-import { HttpResponse } from '../protocols/httpResponse'
+import { ServerError } from '../errors'
+import { HttpResponse } from '../protocols'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
