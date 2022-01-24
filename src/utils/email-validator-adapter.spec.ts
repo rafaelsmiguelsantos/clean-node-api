4ac1981e07b2ea32e586527676edcc3af320db3d
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-01-24 11:36:48 -0300

    chore: pre-push test

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,6 +1,6 @@
 import { EmailValidatorAdapter } from './email-validator'
 
-describe('EmailValidator Adapter', () => {
+describe('EmailValidator Adapters', () => {
   test('Should return false if validator returns false', () => {
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('invalid_email@mail.com')
