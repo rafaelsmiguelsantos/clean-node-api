dbcb10c96d8e2e5634845f467b82e4d7b16ebd99
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-01-26 09:25:52 -0300

    feat: ensure SignUp route returns an account on success

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!<rootDir>/src/**/*index.ts', '!**/protocols/**', '!**/test/**'],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
