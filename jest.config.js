4fbca196a91d5ee38efef72cfcc209c2c3045474
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-01-27 11:51:07 -0300

    chore: add indentation

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!<rootDir>/src/**/*index.ts', '!**/protocols/**', '!**/test/**'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/*index.ts',
+    '!**/protocols/**',
+    '!**/test/**'],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
   testEnvironment: 'node',
