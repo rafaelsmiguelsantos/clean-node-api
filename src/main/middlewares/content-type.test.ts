338e36af71098b4585e74b1e71525389ce78408c
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-01-31 23:25:08 -0300

    refactor: indentation

--- src/main/middlewares/content-type.test.ts
+++ src/main/middlewares/content-type.test.ts
@@ -6,16 +6,20 @@ describe('Body Parser Middleware', () => {
     app.get('/test_content_type', (req, res) => {
       res.send('')
     })
-    await request(app).get('/test_content_type')
+
+    await request(app)
+      .get('/test_content_type')
       .expect('content-type', /json/)
   })
 
-  test('Should return xml content type when forced', async () => {
+  test('Should return xml content tytpe when forced', async () => {
     app.get('/test_content_type_xml', (req, res) => {
       res.type('xml')
       res.send('')
     })
-    await request(app).get('/test_content_type_xml')
+
+    await request(app)
+      .get('/test_content_type_xml')
       .expect('content-type', /xml/)
   })
 })
