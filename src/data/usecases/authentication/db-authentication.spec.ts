deb14ff8371ef866b133295f12b3b04f49d78200
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-03 22:03:52 -0300

    refactor: add makeFakeAuthentication in db authentication spec

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,10 +1,16 @@
 import { AccountModel } from '../../../domain/models/account'
+import { AuthenticationModel } from '../../../domain/usecases/authentication'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
 import { DbAuthentication } from './db-authentication'
 
 const makeFakeAccount = (): AccountModel => ({
   id: 'any_id',
   name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password'
+})
+
+const makeFakeAuthentication = (): AuthenticationModel => ({
   email: 'any_email@mail.com',
   password: 'any_password'
 })
@@ -17,12 +23,13 @@ const makeFakeLoadAccountByEmailRepository = (): LoadAccountByEmailRepository =>
   }
   return new LoadAccountByEmailRepositoryStub()
 }
+
 describe('DbAuthentication UseCases', () => {
   test('Should call LoadAccountByEmailRepository with correct email', () => {
     const loadAccountByEmailRepository = makeFakeLoadAccountByEmailRepository()
     const sut = new DbAuthentication(loadAccountByEmailRepository)
     const loadSpy = jest.spyOn(loadAccountByEmailRepository, 'load')
-    void sut.auth({ email: 'any_email@mail.com', password: 'any_password' })
+    void sut.auth(makeFakeAuthentication())
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
