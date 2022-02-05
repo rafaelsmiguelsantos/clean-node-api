eb1f3e0ea773b65b09f489e17138a4545cd87f5d
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-05 19:36:39 -0300

    refactor: remove attribute private and pass to constructor signup-routes

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -6,9 +6,11 @@ describe('SignUp Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   })
+
   afterAll(async () => {
     await MongoHelper.disconnect()
   })
+
   beforeEach(async () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
