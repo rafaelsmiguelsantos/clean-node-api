2e72bfe6227e7b5f468f0e421a025f78057006b7
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-05 14:58:00 -0300

    test: ensure BcryptAdapter throws if compare throws

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -59,4 +59,14 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'hash')
     await expect(isValid).toBe(false)
   })
+
+  test('Should throw if compare throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.compare('any_value', 'hash')
+    await expect(promise).rejects.toThrow()
+  })
 })
