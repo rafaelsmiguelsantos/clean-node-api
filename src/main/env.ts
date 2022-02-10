14b59ce55e8d2ed66171a6c5cb8093961e40cdbe
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-10 13:24:02 -0300

    refactor: modified file env jwt secret

--- src/main/env.ts
+++ src/main/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl:
-    process.env.MONGO_URL || 'mongodb://localhost:27017/clean-typescript-api',
+  process.env.MONGO_URL || 'mongodb://localhost:27017/clean-typescript-api',
   port: process.env.PORT || 8080,
-  jwt: process.env.JWT_SECRET || 'JASDA1920'
+  jwt: process.env.JWT_SECRET || 'tj670==5H'
 }
