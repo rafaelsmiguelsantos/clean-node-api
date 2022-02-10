ddb1a346383c96649ce2cbbd439d315d85bbe3eb
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-10 11:03:06 -0300

    feat: add LoginFactory

--- src/main/env.ts
+++ src/main/env.ts
@@ -1,5 +1,6 @@
 export default {
   mongoUrl:
     process.env.MONGO_URL || 'mongodb://localhost:27017/clean-typescript-api',
-  port: process.env.PORT || 8080
+  port: process.env.PORT || 8080,
+  jwt: process.env.JWT_SECRET || 'JASDA1920'
 }
