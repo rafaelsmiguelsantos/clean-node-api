41db685c6d7d4b86939d9c1ad3718078847a1591
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-11 15:32:48 -0300

    refactor: add refactor

--- src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter/bcrypt-adapter.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt'
 import { IHashComparer } from '../../../data-layer/usecases/authentication'
 
 export class BcryptAdapter implements Hasher, IHashComparer {
-  constructor(private readonly salt: number) { }
+  constructor (private readonly salt: number) { }
 
   async compare (value: string, hash: string): Promise<boolean> {
     const isValid = await bcrypt.compare(value, hash)
