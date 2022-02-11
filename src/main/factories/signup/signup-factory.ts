c2cea17f7118fe987e62cca45fd010dbf1fec43d
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-11 18:10:46 -0300

    feat: ensure SignUpController calls Authentication with correct values

--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,11 +1,14 @@
 import { DbAddAccount } from '../../../data-layer/usecases/add-account/db-add-account'
+import { DbAuthentication } from '../../../data-layer/usecases/authentication/db-authentication'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
 import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
+import env from '../../env'
 import { makeSignUpValidation } from './signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
@@ -13,9 +16,13 @@ export const makeSignUpController = (): Controller => {
   const bcryptAdapter = new BcryptAdapter(salt)
 
   const accountMongoRepository = new AccountMongoRepository()
+  const jwtAdapter = new JwtAdapter(env.jwt)
+
   const dbAddCcount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+
   const signUpController = new SignUpController(
-    dbAddCcount, makeSignUpValidation())
+    dbAddCcount, makeSignUpValidation(), dbAuthentication)
   const loggerMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpController, loggerMongoRepository)
 }
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -1,10 +1,10 @@
-import { HttpRequest, HttpResponse, Controller, IValidation } from './signup-protocols'
+import { HttpRequest, HttpResponse, Controller, IValidation, Authentication } from './signup-protocols'
 
 import { badRequest, ok, serverError } from '../../helpers/http/http-helper'
 import { IAddAccount } from '../../../domain/usecases/add-account'
 
 export class SignUpController implements Controller {
-  constructor (private readonly addAccount: IAddAccount, private readonly validation: IValidation) { }
+  constructor (private readonly addAccount: IAddAccount, private readonly validation: IValidation, private readonly authentication: Authentication) { }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
@@ -18,6 +18,7 @@ export class SignUpController implements Controller {
         email,
         password
       })
+      await this.authentication.auth({ email, password })
       console.log(account)
       return ok(account)
     } catch (error) {
