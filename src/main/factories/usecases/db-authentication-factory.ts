a005512f0080857a2e226d3d7b549cfa7d7d56b0
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-13 10:02:01 -0300

    refactor: imports edit

--- src/main/factories/usecases/db-authentication-factory.ts
+++ src/main/factories/usecases/db-authentication-factory.ts
@@ -1,8 +1,8 @@
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
-import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 import { DbAuthentication } from '../../../data-layer/usecases/authentication/db-authentication'
 import { IAuthentication } from '../../../domain/usecases/authentication'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adapter/jwt-adapter'
 import env from '../../env'
 
 export const makeDbAuthentication = (): IAuthentication => {
