78758bcdd7ec22be4e15a14eeb101eae174d747c
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-15 23:02:31 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -3,25 +3,24 @@ import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account-mongo-repository'
 
 let accountCollection: Collection
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+beforeEach(async () => {
+  accountCollection = await MongoHelper.getCollection('accounts')
+  await accountCollection.deleteMany({})
+})
+
+const makeSut = (): AccountMongoRepository => {
+  return new AccountMongoRepository()
+}
 
 describe('Account Mongo Repository', () => {
-  beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL)
-  })
-
-  afterAll(async () => {
-    await MongoHelper.disconnect()
-  })
-
-  beforeEach(async () => {
-    accountCollection = await MongoHelper.getCollection('accounts')
-    await accountCollection.deleteMany({})
-  })
-
-  const makeSut = (): AccountMongoRepository => {
-    return new AccountMongoRepository()
-  }
-
   test('Should return an account on add success', async () => {
     const sut = makeSut()
     const account = await sut.add({
@@ -73,5 +72,23 @@ describe('Account Mongo Repository', () => {
     account = await accountCollection.findOne(fakeAccount)
     expect(account).toBeTruthy()
     expect(account.accessToken).toBe('any_token')
+  })
+})
+
+describe('loadByToken()', () => {
+  test('Should return an account on loadByToken success', async () => {
+    const sut = makeSut()
+    await accountCollection.insertOne({
+      name: 'any',
+      email: 'any_email@mail.com',
+      password: 'any_password',
+      accessToken: 'any_token'
+    })
+    const account = await sut.loadByToken('any_token')
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
   })
 })
