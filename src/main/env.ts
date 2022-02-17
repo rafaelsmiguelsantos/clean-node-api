0d3a57107fcf721c9b84ac82a22b4768d5ec6c71
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-16 23:09:57 -0300

    refactor: change the JWT_SECRET

--- src/main/env.ts
+++ src/main/env.ts
@@ -2,5 +2,5 @@ export default {
   mongoUrl:
   process.env.MONGO_URL || 'mongodb://localhost:27017/clean-typescript-api',
   port: process.env.PORT || 8080,
-  jwt: process.env.JWT_SECRET || 'Hj67==5H'
+  jwt: process.env.JWT_SECRET || 'tj67O==5H'
 }
