392261eac1443cb4a416eaddecd1833982e5a149
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-23 09:25:45 -0300

    refactor: includes type Omit in model add-survey

--- src/domain/usecases/add-survey.ts
+++ src/domain/usecases/add-survey.ts
@@ -1,10 +1,6 @@
-import { SurveyAnswerModel } from '@/domain/models/surveys'
+import { SurveyModel } from '@/domain/models/surveys'
 
-export type AddSurveyModel = {
-  question: string
-  answers: SurveyAnswerModel[]
-  date: Date
-}
+export type AddSurveyModel = Omit<SurveyModel, 'id'>
 
 export interface IAddSurvey {
   addSurvey: (data: AddSurveyModel) => Promise<void>
