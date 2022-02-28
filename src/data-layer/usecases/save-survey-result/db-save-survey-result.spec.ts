da04300aecc8f164deca8930e10bad8b60fa0ad2
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-02-28 14:19:37 -0300

    test: ensure SurveyResult returns success

--- src/data-layer/usecases/save-survey-result/db-save-survey-result.spec.ts
+++ src/data-layer/usecases/save-survey-result/db-save-survey-result.spec.ts
@@ -55,4 +55,10 @@ describe('DbSaveSurveyResult Usecase', () => {
     const promise = sut.save(makeFakeSurveyResultData())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return SurveyResult on success', async () => {
+    const { sut } = makeSut()
+    const surveyResultData = await sut.save(makeFakeSurveyResultData())
+    expect(surveyResultData).toEqual(makeFakeSurveyResult())
+  })
 })
