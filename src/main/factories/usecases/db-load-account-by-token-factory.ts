4d6db15366719d6bd12a9d21b0b36acf2a7e4583
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-03-03 11:11:54 -0300

    refactor: move load account by token with folder account

--- src/main/factories/usecases/db-load-account-by-token-factory.ts
+++ src/main/factories/usecases/db-load-account-by-token-factory.ts
@@ -1,6 +1,6 @@
 import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
 import { ILoadAccountByToken } from '@/domain/usecases/load-account-by-token'
-import { DbLoadAccountByToken } from '@/data-layer/usecases/load-account-by-token/db-load-account-by-token'
+import { DbLoadAccountByToken } from '@/data-layer/usecases/account/load-account-by-token/db-load-account-by-token'
 import { JwtAdapter } from '@/infra/criptography/jwt-adapter/jwt-adapter'
 import env from '@/main/env'
 
