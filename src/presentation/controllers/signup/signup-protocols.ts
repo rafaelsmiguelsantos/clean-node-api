54cc8b7816bd0727f46ca4326f9e0fcf359a4eac
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-03-04 09:35:00 -0300

    refactor: add @ import

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols'
-export * from '../../../domain/usecases/account/add-account'
-export * from '../../../domain/usecases/account/authentication'
-export * from '../../../domain/models/account'
+export * from '@/domain/usecases/account/add-account'
+export * from '@/domain/usecases/account/authentication'
+export * from '@/domain/models/account'
