2d3bbbfe8696aecb03a05ba9e17685d4f058c775
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-03-22 16:24:25 -0300

    docs: add login swagger documentation

---
+++ src/main/documentation/paths-swagger/login-path.ts
@@ -0,0 +1,27 @@
+export const loginPath = {
+  post: {
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/loginParams'
+          }
+        }
+      }
+    },
+    tags: ['Login'],
+    sumary: 'API para autenticar usuario',
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/account'
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- src/main/documentation/swagger.ts
+++ src/main/documentation/swagger.ts
@@ -1,3 +1,7 @@
+import { loginPath } from './paths-swagger/login-path'
+import { accountSchema } from './schemas/account'
+import { loginParamsSchema } from './schemas/login-params'
+
 export default {
   openapi: '3.0.0',
   info: {
@@ -10,5 +14,12 @@ export default {
   }],
   tags: [{
     name: 'Login'
-  }]
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
+  }
 }
