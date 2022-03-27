cf51257644e188f6660ed572534c9f17f243ae9a
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-03-27 13:47:27 -0300

    test: ensure DbLoadSurveyResult returns a surveyLoadResult on success

--- src/data-layer/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ src/data-layer/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,5 +1,7 @@
 import { ILoadSurveyResultRepository } from '@/data-layer/protocols/db/load-survey-result-repository'
 import { mockLoadSurveyResultRepository } from '@/data-layer/test'
+import { mockSurveyResultModel } from '@/domain/test'
+import { throwNewError } from '@/domain/test/tests-helpers'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 
 type SutTypes = {
@@ -7,7 +9,7 @@ type SutTypes = {
   loadSurveyResultRepositoryStub: ILoadSurveyResultRepository
 }
 
-const makeSut = (): SutTypes => {
+const mockSut = (): SutTypes => {
   const loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
   const sut = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
   return {
@@ -18,9 +20,22 @@ const makeSut = (): SutTypes => {
 
 describe('DbLoadSurveyResult UseCase', () => {
   test('Should call LoadSurveyResultRepository', async () => {
-    const { sut, loadSurveyResultRepositoryStub } = makeSut()
+    const { sut, loadSurveyResultRepositoryStub } = mockSut()
     const loadBySurveyIdSpy = jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
     await sut.load('any_survey_id')
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id')
+  })
+
+  test('Should throw if LoadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = mockSut()
+    jest.spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId').mockReturnValueOnce(Promise.reject(throwNewError))
+    const promise = sut.load('any_survey_id')
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return surveyResultModel on success', async () => {
+    const { sut } = mockSut()
+    const result = await sut.load('any_survey_id')
+    expect(result).toEqual(mockSurveyResultModel())
   })
 })
