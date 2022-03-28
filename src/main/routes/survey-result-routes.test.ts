237bee49590feb53bf43e3355eca5e3041b20da5
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-03-28 09:37:54 -0300

    test: ensure load survey rout returns 403 if no accessToken is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -43,24 +43,51 @@ describe('Survey Result Routes', () => {
         .send({ answer: 'any_answer' })
         .expect(403)
     })
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await mockAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      await request(app)
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({ answer: 'Answer 1' })
+        .expect(200)
+    })
   })
 
-  test('Should return 200 on save survey result with accessToken', async () => {
-    const accessToken = await mockAccessToken()
-    const res = await surveyCollection.insertOne({
-      question: 'Question',
-      answers: [{
-        answer: 'Answer 1',
-        image: 'http://image-name.com'
-      }, {
-        answer: 'Answer 2'
-      }],
-      date: new Date()
+  describe('GET /surveys/:surveyId/results', () => {
+    test('Should return 403 on load survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .expect(403)
     })
-    await request(app)
-      .put(`/api/surveys/${res.ops[0]._id}/results`)
-      .set('x-access-token', accessToken)
-      .send({ answer: 'Answer 1' })
-      .expect(200)
+
+    test('Should return 200 on save survey result with accessToken', async () => {
+      const accessToken = await mockAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        }, {
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      await request(app)
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({ answer: 'Answer 1' })
+        .expect(200)
+    })
   })
 })
