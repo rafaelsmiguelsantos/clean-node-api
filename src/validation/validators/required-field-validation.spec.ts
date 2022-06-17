fc687c1698683161cc6a1d5780176b531c7478db
rafaelsmiguelsantos <miguel.desenvolvimento@hotmail.com>
2022-06-17 09:15:59 -0300

    test: gitHub Copilot

--- src/validation/validators/required-field-validation.spec.ts
+++ src/validation/validators/required-field-validation.spec.ts
@@ -14,10 +14,4 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ invalidField: faker.random.word() })
     expect(error).toEqual(new MissingParamError(field))
   })
-
-  test('Should not return if validation succeeds', () => {
-    const sut = makeSut()
-    const error = sut.validate({ [field]: faker.random.word() })
-    expect(error).toBeFalsy()
-  })
 })
